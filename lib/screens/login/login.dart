2982924bd661bb720a99f632aebab3f1c935faf8
sabrinaatha <sabrinaathashania@gmail.com>
2023-12-14 21:01:00 +0700

    change login

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,19 +1,31 @@
+import 'package:flutter/gestures.dart';
 import 'package:mutualibri/components/already_account.dart';
 import 'package:mutualibri/constants.dart';
-import 'package:mutualibri/menu.dart';
-import 'package:mutualibri/models/review.dart';
 import 'package:mutualibri/screens/list_product.dart';
-import 'package:mutualibri/screens/login/login_screen_top_image.dart';
 import 'package:flutter/material.dart';
-import 'package:mutualibri/widgets/catalog.dart';
+import 'package:mutualibri/screens/login/login_screen_top_image.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:mutualibri/responsive.dart';
-import 'package:mutualibri/components/background.dart';
-import 'package:mutualibri/screens/review_list.dart';
+
+import '../../../../constants.dart';
 
 void main() {
-  runApp(const LoginPage());
+  runApp(const LoginApp());
+}
+
+class LoginApp extends StatelessWidget {
+  const LoginApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+      ),
+      home: const LoginPage(),
+    );
+  }
 }
 
 class LoginPage extends StatefulWidget {
@@ -31,86 +43,77 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
       body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             LoginScreenTopImage(),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 700),
-              child: TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                ),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
               ),
             ),
             const SizedBox(height: 12.0),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 700),
-              child: TextField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                ),
-                obscureText: true,
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
               ),
+              obscureText: true,
             ),
-            const SizedBox(height: 16.0),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 700),
-              child: ElevatedButton(
-                onPressed: () async {
-                  String username = _usernameController.text;
-                  String password = _passwordController.text;
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () async {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
 
-                  final response =
-                      await request.login("http://127.0.0.1:8000/auth/login/", {
-                    'username': username,
-                    'password': password,
-                  });
+                // Cek kredensial
+                // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                // Untuk menyambungkan Android emulator dengan Django pada localhost,
+                // gunakan URL http://10.0.2.2/
+                final response = await request.login(
+                    "https://mutualibri-a08-tk.pbp.cs.ui.ac.id/auth/login/", {
+                  'username': username,
+                  'password': password,
+                });
 
-                  if (request.loggedIn) {
-                    String message = response['message'];
-                    String uname = response['username'];
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyHomePage()),
-                    );
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(SnackBar(
-                          content: Text("$message Selamat datang, $uname.")));
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('Login Gagal'),
-                        content: Text(response['message']),
-                        actions: [
-                          TextButton(
-                            child: const Text('OK'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
+                if (request.loggedIn) {
+                  String message = response['message'];
+                  String uname = response['username'];
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProductPage()),
+                  );
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(SnackBar(
+                        content: Text("$message Selamat datang, $uname.")));
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Login Gagal'),
+                      content: Text(response['message']),
+                      actions: [
+                        TextButton(
+                          child: const Text('OK'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: const Text('Login'),
             ),
-            const SizedBox(height: defaultPadding),
+             const SizedBox(height: defaultPadding),
             AlreadyHaveAnAccountCheck(
               press: () {
                 // Navigator.push(
@@ -122,7 +125,7 @@ class _LoginPageState extends State<LoginPage> {
                 //   ),
                 // );
               },
-            ),
+            )
           ],
         ),
       ),
